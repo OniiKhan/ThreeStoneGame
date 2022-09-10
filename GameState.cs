cb921aad83e809654008e570c8ebaff894b2b22e
OniiKhan <104305191+OniiKhan@users.noreply.github.com>
2022-09-10 20:30:02 +0300

    Add files via upload

---
+++ GameState.cs
@@ -0,0 +1,167 @@
+﻿using System;
+
+namespace _3Tas
+{
+    public enum Player
+    {
+        None, Red, Blue
+    }
+    public class GameState
+    {
+        public event Action<int, int, int, int> MoveMade;
+        public event Action GameEnded;
+        public event Action GameRestarted;
+
+        public Player[,] Board { get; private set; }
+        public Player CurrentPlayer { get; set; }
+        
+        private short TurnPassed { get; set; }
+        private bool Selection { get; set; }
+        public bool GameOver { get; private set; }
+        private int PosR { get; set; }
+        private int PosC { get; set; }
+
+        public GameState()
+        {
+            Board = new Player[3, 3];
+            CurrentPlayer = Player.Red;
+            TurnPassed = 0;
+            Selection = false;
+            GameOver = false;
+        }
+
+        private void SwitchPlayer()
+        {
+            CurrentPlayer = CurrentPlayer == Player.Red ? Player.Blue : Player.Red;
+        }
+        private bool IsEmpty(int r, int c)
+        {
+            if (Board[r, c] == Player.None) 
+            {
+                return true;
+            }
+            return false;
+        }
+        private bool IsYours(int r, int c)
+        {
+            if (Board[r,c] == CurrentPlayer)
+            {
+                return true;
+            }
+            return false;
+        }
+        private bool IsNear(int r, int c)
+        {
+            if ((PosC == c && Math.Abs(PosR - r) == 1) || (PosR == r && Math.Abs(PosC - c) == 1)) 
+            {
+                return true;
+            }
+            return false;
+        }
+        private bool IsLocked(int r, int c)
+        {
+            if (Board[r, c == 0 ? 0 : c - 1] == Player.None ||
+                Board[r, c == 2 ? 2 : c + 1] == Player.None ||
+                Board[r == 0 ? 0 : r - 1, c] == Player.None ||
+                Board[r == 2 ? 2 : r + 1, c] == Player.None)
+            {
+                return false;
+            }
+            return true;
+        }
+        private bool AreSquaresMarked((int, int)[] squares, Player player)
+        {
+            foreach ((int r, int c) in squares)
+            {
+                if (Board[r, c] != player)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        private bool DidMoveWin(int r, int c)
+        {
+            (int, int)[] row = new[] { (r, 0), (r, 1), (r, 2) };
+            (int, int)[] col = new[] { (0, c), (1, c), (2, c) };
+            if (AreSquaresMarked(row, CurrentPlayer) || AreSquaresMarked(col, CurrentPlayer))
+            {
+                GameOver = true;
+                return true;
+            }
+            return false;
+        }
+        private void PlaceStone(int r, int c)
+        {
+            if (IsEmpty(r, c))
+            {
+                Board[r, c] = CurrentPlayer;
+                
+                TurnPassed++;
+                MoveMade?.Invoke(-1, 0, r, c);
+                if (DidMoveWin(r, c))
+                {
+                    GameEnded?.Invoke();
+                    return;
+                }
+                SwitchPlayer();
+            }
+        }
+        private void SelectStone(int r, int c)
+        {
+            if (IsYours(r, c) && !IsLocked(r, c))
+            {
+                PosR = r;
+                PosC = c;
+                Selection = true;
+                MoveMade?.Invoke(0, -1, r, c);
+            }
+        }
+        private void SelectPlace(int r, int c)
+        {
+            if (PosR == r && PosC == c)
+            {
+                Selection = false;
+                MoveMade?.Invoke(-1, -1, r, c);
+            }
+            else if (IsEmpty(r, c) && IsNear(r, c))
+            {
+                Board[r, c] = CurrentPlayer;
+                Board[PosR, PosC] = Player.None;
+                
+                Selection = false;
+                MoveMade?.Invoke(PosR, PosC, r, c);
+                if (DidMoveWin(r, c))
+                {
+                    GameEnded?.Invoke();
+                    return;
+                }
+                SwitchPlayer();
+            }
+        }
+        public void MakeMove(int r, int c)
+        {
+            if (TurnPassed < 6)
+            {
+                PlaceStone(r, c);
+            }
+            else if (!Selection)
+            {
+                SelectStone(r, c);
+            }
+            else
+            {
+                SelectPlace(r, c);
+            }
+        }
+        public void Reset()
+        {
+            Board = new Player[3, 3];
+            CurrentPlayer = Player.Red;
+            TurnPassed = 0;
+            GameOver = true;
+            GameRestarted?.Invoke();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace _3Tas
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly Dictionary<Player, ImageSource> imageSources = new()
+        {
+            { Player.Red, new BitmapImage(new Uri("pack://application:,,,/Assets/Red.png")) },
+            { Player.Blue, new BitmapImage(new Uri("pack://application:,,,/Assets/Blu.png")) },
+            { Player.None, new BitmapImage()}
+        };
+
+        private readonly GameState GameState = new();
+        private readonly Image[,] imageControls = new Image[3, 3];
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetupGameGrid();
+
+            GameState.MoveMade += OnMoveMade;
+            GameState.GameEnded += OnGameEnded;
+            GameState.GameRestarted += OnGameRestarted;
+        }
+
+        private  void OnGameRestarted()
+        {
+            for (int r = 0; r < 3; r++)
+            {
+                for (int c = 0; c < 3; c++)
+                {
+                    imageControls[r, c].Source = null;
+                }
+            }
+            EndScreen.Visibility = Visibility.Hidden;
+        }
+
+        private async void OnGameEnded()
+        {
+            await Task.Delay(1000);
+            EndScreen.Visibility = Visibility.Visible;
+            WinnerImage.Source = imageSources[GameState.CurrentPlayer];
+        }
+
+        private void SetupGameGrid()
+        {
+            for (int r = 0; r < 3; r++)
+            {
+                for (int c = 0; c < 3; c++)
+                {
+                    Image imageControl = new();
+                    GameGrid.Children.Add(imageControl);
+                    imageControls[r, c] = imageControl;
+                }
+            }
+        }
+
+        private void OnMoveMade(int r0, int c0, int r, int c)
+        {
+            if (r0 == -1 && c0 == -1)
+            {
+                imageControls[r, c].Opacity = 1;
+                return;
+            }
+            if (c0 == -1)
+            {
+                imageControls[r, c].Opacity = 0.5;
+                return;
+            }
+            if (r0 != -1)
+            {
+                imageControls[r0, c0].Source = imageSources[Player.None];
+                imageControls[r0, c0].Opacity = 1;
+            }
+            Player player = GameState.Board[r, c];
+            imageControls[r, c].Source = imageSources[player];
+        }
+
+        private void GameGrid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            double squareSize = GameGrid.Width / 3;
+            Point click = e.GetPosition(GameGrid);
+            int row = (int)(click.Y / squareSize);
+            int col = (int)(click.X / squareSize);
+            GameState.MakeMove(row, col);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (GameState.GameOver)
+            {
+                GameState.Reset();
+            }
+        }
+    }
+}
